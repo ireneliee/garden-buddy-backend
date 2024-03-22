61c6c5762122bb51f1f9e0ec617ab2d6605471f6
kohwenjie <70895902+kohwenjie@users.noreply.github.com>
2024-03-22 17:58:21 +0800

    testing

--- root.py
+++ root.py
@@ -1,8 +1,36 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-flask_app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/garden_buddy_database'
+db = SQLAlchemy(app)
 
-@flask_app.route('/')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username    
+
+@app.route('/')
 def index():
-	return render_template('index.html', title = "Garden Buddy Homepage", content = "I love Garden Buddy")
+    return render_template('index.html', title="Garden Buddy Homepage", content="I love Garden Buddy")
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.username for user in users])
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    new_user = User(username=data['username'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=False)
